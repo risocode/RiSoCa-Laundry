c89a4ee2c8f345be723c69dfc4836b20f5d8cb1c
Risoca Developer <risoca.developer@gmail.com>
2025-12-07 18:53:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3277782862-3b56d",
+  "appId": "1:717110242884:web:dbf30fb5b41c8b8a3dd611",
+  "apiKey": "AIzaSyCO7fJoO0l05HHp1RTKzw1YJHpxj1prEfw",
+  "authDomain": "studio-3277782862-3b56d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "717110242884"
+};
